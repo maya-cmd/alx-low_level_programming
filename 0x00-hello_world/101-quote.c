9bd9c3fa44065a33e65476c7b05e8738cb647ab6
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-13 19:55:38 -0700

    Corrections made to file 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * standard error
- * Return 1 (success)
+ * main - Entry Point
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-char quo[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n":
-write(1.quo.59):	
-return (1):
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
+
