74b93f892ee6a23228f0d9f24aae3c7fab92ea32
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-13 17:22:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,5 +6,9 @@
  */
 int main(void)
 {
-    printf("size of char: %d byte(s), sizeofchar\n");
-    return (0);
+printf("size of a char: %lu byte(s)\n", sizeof(char));
+printf("size of an int: %lu byte(s)\n", sizeof(int));
+printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("size of a long long: %lu byte(s)\n", sizeof(long long int));
+printf("size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
