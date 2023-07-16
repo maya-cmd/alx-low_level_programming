236a910da04e91ef3326f660383831e6345c6f3f
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-16 03:19:35 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - Entry point
+ *iterare over the lowercase alphabet characters and print
+ * check for conditions
+ * output results
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'z'; c <= 'a'; c--)
+{
+	putchar (c);
+}
+putchar('\n');
+return (0);
+}
