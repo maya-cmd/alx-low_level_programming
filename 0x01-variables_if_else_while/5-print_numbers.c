adb1fdb17d5abe3472af2decc62e10e92d0e2e73
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-16 00:20:08 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *iterare over the numbers and print
+ * output results
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int i = 0; i < 10; i++)
+printf("%d", i);
+}
