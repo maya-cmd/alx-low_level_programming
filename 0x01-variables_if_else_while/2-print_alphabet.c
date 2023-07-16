77c5c046beb9e1d56ff8cf863aaf1f367d690e61
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-15 22:30:34 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - Entry point
+ *iterare over the alphabet characters and print
+ change to lowercase
+ print using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c, result;
+
+for (c = 'A'; c < 'Z'; c++)
+result = tolower(c);
+putchar (result);
+return (0);
+}
