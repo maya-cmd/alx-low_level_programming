c6fab3e37e95f24fbc48b0a6fecfdbab216b813f
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-18 09:16:27 -0700

    Updated 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
+ * The main.h header that contains the prototype is included
  * _putchar - writes the character c to stdout
  *
- * Return: On success 1.
+ * The function iterates through the letters
+ * and prints characters using _putchar.
  */
 void print_alphabet(void)
 {
