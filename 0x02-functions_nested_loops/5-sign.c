dbb195802878b61252bb5c67b3eecd149192c011
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-19 06:21:13 -0700

    Printing number signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,18 +13,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
-	{
-		_putchar(54);
-		return (-1);
-	}
-	else
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
 	}
-	_putchar('\n');
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+	return (-1);
 }
