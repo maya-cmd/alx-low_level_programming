caeba532710e4b99c600a05c36675735bcd3d405
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-19 15:14:41 -0700

    more corrections on 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,14 +10,15 @@ int main(void)
 int i = 0;
 long j = 1, k = 2, sum = k;
 
-while (k + j < 4000000)
+while (k + j <= 4000000)
 {
-k == j;
-if (k % j == 0)
+k = j;
+if (k % 2 == 0)
 sum += k;
-j = k - j;
+j = k + j;
 ++i;
 }
 printf("%ld\n", sum);
 return (0);
 }
+
