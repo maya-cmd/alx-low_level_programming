4dc58fd56961dff118d12b044a7cc1d2cc93452b
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-21 14:14:02 -0700

    Updated printing integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,38 +8,15 @@
  */
 void print_number(int n)
 {
-long m;
-int c;
-long num;
-
-num = n;
-if (num < 0)
+unsigned int k = n;
+if (n < 0)
 {
-num *= -1;
+n *= -1;
+k = n;
 _putchar('-');
 }
-
-m = 1;
-c = 1;
-while (c)
-{
-if (num / (m * 10) > 0)
-m *= 10;
-else
-c = 0;
-}
-
-while (num >= 0)
-{
-if (m == 1)
-{
-_putchar(num % 10 + '0');
-num = -1;
-}
-else
-{
-_putchar((num / m % 10) + '0');
-m /= 10;
-}
-}
+k /= 10;
+if (k != 0)
+print_number(k);
+_putchar((unsigned int) n % 10 + '0');
 }
