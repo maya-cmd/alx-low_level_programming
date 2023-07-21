4d384386b3b3c942779b0389882f8a6ce2517c35
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-21 06:26:47 -0700

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line- Draws a straight line in the terminal
+ * @n : the number of times the character _ should be printed
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('-');
+		}
+		_putchar('\n');
+	}
+}
