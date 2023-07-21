04e04f6175a435cbc3096aa7854797dc791105d7
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-21 13:02:13 -0700

    making fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,32 +2,30 @@
 
 /**
  * main - prints the numbers from 1 to 100
- *
+ *Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz.
+ * Multiples of both 3 and 5 should print FizzBuzz.
  *
  * Return: Always 0.
  */
 int main(void)
 {
 int i;
+
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz ");
-}
+printf("FizzBuzz");
 else if (i % 3 == 0)
-{
-printf("Fizz ");
-}
+printf("Fizz");
 else if (i % 5 == 0)
-{
-printf("Buzz ");
-}
+printf("Buzz");
 else
-{
-printf("%d ", i);
-}
+printf("%d", i);
+if (i != 100)
+printf(" ");
+i++;
 }
 printf("\n");
+
 return (0);
 }
