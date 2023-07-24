a4fdc068aa5cc100458b5bbb53fbaa44473da785
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-24 06:24:50 -0700

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen -  Returns the length of a string
+ *@s : Input
+ * Return: len.
+ */
+
+int _strlen(char *s)
+{
+int len;
+
+for (len = 0; s[len] != '\0'; len++)
+{
+
+}
+return (len);
+}
