e720ac46cdd609d9860cd153a9879c2f90cab73d
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-24 18:00:20 -0700

    Half a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str : The string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int a, b, len;
+
+len = 0;
+
+for (a = 0; str[a] != '\0'; a++)
+{
+len++;
+}
+b = (len / 2);
+if ((len % 2) == 1)
+b =  ((len + 1) / 2);
+for (a = b; str[a] != '\0'; a++)
+{
+_putchar(str[a]);
+}
+_putchar('\n');
+}
+
