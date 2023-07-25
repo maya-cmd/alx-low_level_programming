5b0254db07166a2b8b57119aff2b5d5f28e784f7
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-24 18:48:38 -0700

    Updated copy to another

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,17 +9,15 @@
 char *_strcpy(char *dest, char *src)
 {
 int i = 0;
-int x = 0;
+int x;
 
-while (*(src + 1) != '\0')
+while (src[i] != '\0')
 {
 i++;
 }
-for ( ; x < i; x++)
+for (x = 0; x <= i; x++)
 {
 dest[x] = src[x];
 }
-dest[i] = '\0';
 return (dest);
 }
-
