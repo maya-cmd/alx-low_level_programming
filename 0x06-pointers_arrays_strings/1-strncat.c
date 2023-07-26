826b5e2852206ac9e6d5cd3009b9b4d235b2008c
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-26 13:11:46 -0700

    Append string to another

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest : First string
+ * @src : Second string to be appended to dest
+ * @n: Describes number of characters needed to append from src to dest
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+
+for (i = 0; dest[i] != '\0'; i++)
+{
+
+}
+for (j = 0; j < n; j++)
+{
+dest[i] = src[j];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
