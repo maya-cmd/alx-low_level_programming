02ca0443321e312b5fed093559716903f4a8b2a0
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-27 00:48:16 -0700

    copying a string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,14 +8,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
-for (i = 0; i < n && src[i] != '\0'; i++)
-{
-dest[i] = src[i];
-}
-for (i = 0; i < n; i++)
-{
-dest[i] = '\0';
-}
-return (dest);
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
