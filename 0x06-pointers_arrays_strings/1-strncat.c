11008c4a0978b15e6d2d38dd7714e27f14813e57
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-07-27 01:06:53 -0700

    Updated copying strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * _strncat - concatenates two strings
+ * _strncat - Concatenates two strings
  * @dest : First string
  * @src : Second string to be appended to dest
  * @n: Describes number of characters needed to append from src to dest
  * Return: Always 0.
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,19 +10,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i, j;
-
-for (i = 0; dest[i] != '\0'; i++)
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
-
+dest[i] = src[i];
 }
-
-for (j = 0; j < n && src[j] != '\0'; j++)
+for (i = 0; i < n; i++)
 {
-dest[j] = src[j];
+dest[i] = '\0';
 }
-
-dest[j] = '\0';
-
 return (dest);
 }
