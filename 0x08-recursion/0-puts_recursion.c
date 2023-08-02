1a7c6de48b008908ec5f2f4e831b9f9ab6ce0d34
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-08-02 06:04:51 -0700

    Making a string using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts_recursion - The function prints a string
+ * @s : The string to be printed
+ *
+ *Return : Always 0
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Tasks on recursive functions in C
