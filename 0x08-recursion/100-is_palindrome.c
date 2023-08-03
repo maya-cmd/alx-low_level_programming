3242b510591c922a1b10ebaabc99e665411be3a2
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-08-03 01:46:13 -0700

    Updated palindrome string

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -46,7 +46,7 @@ int compare_character(char *s, int num1, int num2)
 	if (*(s + num1) == *(s + num2))
 	{
 		if (num1 == num2 || num1 == num2 + 1)
-			return (0 + compare_character(s, num1 + 1, num2 - 1));
+			return (compare_character(s, num1 + 1, num2 - 1));
 	}
-	return (0);
+	return (1);
 }
