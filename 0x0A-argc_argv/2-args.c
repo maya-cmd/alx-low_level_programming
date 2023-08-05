bcaac86dbc2401fde2db164d14e93b4de8844890
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-08-04 18:19:51 -0700

    Arguments received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - The function outputs the arguments it gets
+ *@argc : Argument count
+ *@argv :Argument vector
+ *
+ *Return: Always 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
