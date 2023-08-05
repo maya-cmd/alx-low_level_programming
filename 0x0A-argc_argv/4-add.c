3ff6b62031f9ecf19653a24d726a5729327bb3f6
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-08-04 19:47:54 -0700

    sum

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
 
 /**
  *main - The function adds positive numbers
@@ -15,23 +15,21 @@ int main(int argc, char *argv[])
 {
 	int i;
 	unsigned int n, addition = 0;
-	char *x;
 
 	if (argc > 1)
 	{
-		for (i = 0; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
-			x = argv[i];
-			for (n = 0; n < strlen(x); n++)
+			for (n = 0; argv[i][n] != '\0'; n++)
 			{
-				if (x[n] < '0' || x[n] > '9')
+				if (!isdigit(argv[i][n]))
 				{
-					printf("Error\n");
+					printf("Error: %s\n", argv[i]);
 					return (1);
 				}
 			}
 
-			addition += atoi(x);
+			addition += atoi(argv[i]);
 		}
 		printf("%d", addition);
 	}
