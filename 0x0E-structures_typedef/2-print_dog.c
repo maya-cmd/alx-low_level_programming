566a787adab2ececbfe7c23c1ea5aeb8e88e4c4e
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-08-14 07:24:38 -0700

    Print gog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ *print_dog - Function prints a struct dog
+ *
+ *@d: struct to be printed
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+		d->name = "(nil)";
+		}
+		if (d->owner == NULL)
+		{
+		d->owner = "(nil)";
+		}
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+}
+
+
