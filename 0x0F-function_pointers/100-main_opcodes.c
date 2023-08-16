532d7f40ecf0b824ecd9118b86dacddc8f434ecb
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-08-16 12:23:26 -0700

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Function prints the opcodes of its own main function
+ *@argc: The number of arguments
+ *@argv: The argument array
+ *
+ *Return:0 on success
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, n = 0;
+	char *mem;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	mem = (char *)main;
+
+	while (n < bytes)
+	{
+		if (n == bytes - 1)
+		{
+			printf("%.2x\n", mem[n]);
+		}
+		else
+		{
+			printf("%.2x ", mem[n]);
+		}
+		n++;
+	}
+}
