d3ad3bb754bdbdebbecce9d83bc4fe96bd565f49
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-08-29 18:53:48 -0700

    Updated add node at a given point.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = newestnode;
 		return (newestnode);
 	}
-	while (temp && j < idx)
+	while (temp && j < idx - 1)
 	{
 		temp = temp->next;
 		j++;
