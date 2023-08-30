f61e1d2e77dc9a7fb91b57606e339d35c130d5d6
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-08-29 18:57:58 -0700

    Add node at a given point.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newestnode = malloc(sizeof(listint_t));
 	if (!newestnode)
 		return (NULL);
-	if (!*head)
+	if (!head)
 		return (NULL);
 
 	newestnode->n = n;
