128ffa0e9492d6efdefae9da8fca4d862fe8df17
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-08-30 07:06:03 -0700

    Free listint

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ *free_listint_safe - Function frees listint_t list
+ *@h: The head of listint_t
+ *
+ *Return: The size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *temp;
+	size_t freed_count = 0;
+	size_t address_difference;
+
+	if (!h || !*h)
+		return (0);
+
+
+	while (*h)
+	{
+		address_difference = *h - (*h)->next;
+		if (address_difference > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			freed_count++;
+		}
+		else
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			freed_count++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (freed_count);
+}
+
+
