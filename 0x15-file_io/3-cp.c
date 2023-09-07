e2a7da26c397338c9d0aaf93fb80d9e48ae97e6d
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-09-07 02:08:43 -0700

    Copy to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,51 +28,52 @@ void close_open_files(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, read_result, write_result;
-	char *buffer[1024];
-	char *file_from;
-	char *file_to;
+	int file_from, file_to, read_result, write_result;
+	char *buffer;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	file_from = argv[1];
-	file_to = argv[2];
 
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Memory allocation failed\n");
+		exit(1);
 	}
 
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
-		exit(99);
-	}
+	file_from = open(argv[1], O_RDONLY);
+	read_result = read(file_from, buffer, 1024);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		read_result = read(fd_from, buffer, sizeof(buffer));
-		if (read_result == -1)
+		if (file_from == -1 || read_result == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
 			exit(98);
 		}
 
-		write_result = write(fd_to, buffer, read_result);
-		if (write_result == -1)
+		write_result = write(file_to, buffer, read_result);
+		if (file_to == -1 || write_result == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
 			exit(99);
 		}
+
+		read_result = read(file_from, buffer, 1024);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
+
 	} while (read_result > 0);
 
-	close_open_files(fd_from);
-	close_open_files(fd_to);
+	close_open_files(file_from);
+	close_open_files(file_to);
 
 	return (0);
 }
