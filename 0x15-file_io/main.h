42a21207054b42dd060024ace5a6db854f91563b
Sheelah Mogaka@.com <sheelah.mogaka@.com>
2023-09-07 11:30:20 -0700

    Copy file to another

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUFFER_SIZE 1024
+/*
+ * File: main.h
+ * Auth: Sheelah Mogaka
+ * Desc: Header file with prototypes of all the functions in the
+ * 0x15-file_io directory
+ */
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,4 +22,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* MAIN_H */
