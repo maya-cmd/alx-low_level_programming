db12314941849ee4eeb716e7c2b818e397026fbf
sheelah <sheelah.mogaka@gmail.com>
2023-10-02 22:18:40 -0700

    Elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,13 +2,13 @@
 #define MAIN_H
 
 
-#include <stdio.h>
-#include "main.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
+#include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
