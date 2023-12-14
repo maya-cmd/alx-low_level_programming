54e9dc8116ca5fe8b92272114de876f0231d7e5b
Sheelah <sheelah.mogaka@gmail.com>
2023-12-14 14:13:10 -0800

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *get_endianness - The function searches for the endianness
+ *
+ *Return: endian small if 1 and big endian if 0
+ *
+ *
+ */
+int get_endianness(void)
+{
+	int i = 1;
+
+
+	if (*(char *)&i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
