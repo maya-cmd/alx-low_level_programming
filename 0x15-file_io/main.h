a279575ed5fbbdc67bc609ce365e3302ffbbabce
Sheelah <sheelah.mogaka@gmail.com>
2023-12-18 04:57:09 -0800

    cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <stdlib.h>
+#include <stdio.h>
 int _putchar(char c);
 size_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
