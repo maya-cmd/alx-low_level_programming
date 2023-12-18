a6d5714eee02c394a50180099297b7fda5df2448
Sheelah <sheelah.mogaka@gmail.com>
2023-12-18 04:12:53 -0800

    Create file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 size_t read_textfile(const char *filename, size_t letters);
